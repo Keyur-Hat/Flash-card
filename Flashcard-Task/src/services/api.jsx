f83a8577f3063ffff23150c0ed81d392f5a9ad56
Keyur Kulkarni <keyurkwo@gmail.com>
2024-08-11 19:17:07 +0530

    Components

---
+++ Flashcard-Task/src/services/api.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:3308' 
+})
+
+export default api;
+
+export const getFlashcards = async () => {
+    return api.get('/flashcards');
+};
+
+export const addFlashcard = async (flashcard) => {
+    return api.post('/flashcards', flashcard);
+};
+
+export const deleteFlashcard = async (id) => {
+    return api.delete(`/flashcards/${id}`);
+
+};
+
+export const updateFlashcard = async (id, flashcard) => {
+    return api.put(`/flashcards/${id}`, flashcard);
+};
--- server/server.js
+++ server/server.js
@@ -39,6 +39,48 @@ app.get('/api/flashcards', (req, res) => {
     });
 })
 
+app.post('/api/flashcards', (req, res) => {
+    const { question, answer } = req.body;
+    const query = 'INSERT INTO flashcards (question, answer) VALUES (?,?)';
+    db.query(query, [question, answer], (err, result) => {
+        if(err) {
+            console.error('Error inserting flashcard:', err);
+            res.status(500).send('Error inserting flashcard');
+          } else {
+            res.send("Flashcard inserted successfully");
+          }
+    });
+})
+
+app.put('/api/flashcards/:id', (req, res) => {
+    const { id } = req.params;
+    const { question, answer } = req.body;
+    const query = 'UPDATE flashcards SET question=?, answer=? WHERE id=?';
+    db.query(query, [question, answer, id], (err, result) => {
+        if(err) {
+            console.error('Error updating flashcard:', err);
+            res.status(500).send('Error updating flashcard');
+          } else {
+            res.send("Flashcard updated successfully");
+          }
+    });
+ 
+})
+
+app.delete('/api/flashcards/:id', (req, res) => {
+    const { id } = req.params;
+    const query = 'DELETE FROM flashcards WHERE id=?';
+    db.query(query, [id], (err, result) => {
+        if(err) {
+            console.error('Error deleting flashcard:', err);
+            res.status(500).send('Error deleting flashcard');
+          } else {
+            res.send('Flashcard deleted successfully');
+          }
+    });
+    
+})
+
 
 
 app.listen(3308, () => console.log('Server running on port 3308'));
